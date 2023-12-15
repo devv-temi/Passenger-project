b02af24fe90318355def8340fcf9789d88b9f381
James Balogun <jamesbalogun085@gmail.com>
2023-12-15 16:26:20 +0100

    Updated boarding app

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let countEL = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 console.log(countEL)
 
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEL.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+
+    console.log(count)
 }
